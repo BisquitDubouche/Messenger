cf4ce3e9ee2ce48d19a866c857c620afc1cb0c73
BisquitDubouche <acapheles@gmail.com>
2023-05-08 18:40:07 +0600

    Finished registration interface, Connected mongodb and schemas configured (installed some authorization packages)

--- app/(site)/components/AuthForm.tsx
+++ app/(site)/components/AuthForm.tsx
@@ -1,9 +1,11 @@
 "use client";
 
-import Button from '@/app/components/Button';
+import Button from "@/app/components/Button";
 import Input from "@/app/components/input/Input";
+import AuthSocialButton from "./AuthSocialButton";
 import { useCallback, useState } from "react";
 import { FieldValues, useForm, SubmitHandler } from "react-hook-form";
+import { BsGithub, BsGoogle } from "react-icons/bs";
 
 type Variant = "LOGIN" | "REGISTER";
 
@@ -57,41 +59,95 @@ const AuthForm = () => {
     >
       <div
         className='
-      bg-white
-      px-4
-      py-8
-      shadow
-      sm:rounded-lg
-      sm:px-10
+        bg-white
+        px-4
+        py-8
+        shadow
+        sm:rounded-lg
+        sm:px-10
       '
       >
-        <form className='space-6' onSubmit={handleSubmit(onSubmit)}>
+        <form className='space-y-6' onSubmit={handleSubmit(onSubmit)}>
           {variant === "REGISTER" && (
             <Input
               id='name'
               label='Name'
               register={register}
               errors={errors}
+              disabled={isLoading}
             />
           )}
-           <Input
-              id='email'
-              label='Email'
-              type='email'
-              register={register}
-              errors={errors}
-            />
-            <Input
-              id='password'
-              label='Password'
-              type='password'
-              register={register}
-              errors={errors}
-            />
-            <div>
-                <Button>Test</Button>
-            </div>
+          <Input
+            id='email'
+            label='Email'
+            type='email'
+            register={register}
+            errors={errors}
+            disabled={isLoading}
+          />
+          <Input
+            id='password'
+            label='Password'
+            type='password'
+            register={register}
+            errors={errors}
+            disabled={isLoading}
+          />
+          <div>
+            <Button disabled={isLoading} fullWidth type='submit'>
+              {variant === "LOGIN" ? "Sign In" : "Register"}
+            </Button>
+          </div>
         </form>
+        <div className='mt-6'>
+          <div className='relative'>
+            <div
+              className='
+                absolute 
+                inset-0 
+                flex 
+                items-center
+              '
+            >
+              <div className='w-full border-t border-gray-300' />
+            </div>
+            <div className='relative flex justify-center text-sm'>
+              <span className='bg-white px-2 text-gray-500'>
+                Or continue with
+              </span>
+            </div>
+          </div>
+
+          <div className='mt-6 flex gap-2'>
+            <AuthSocialButton
+              icon={BsGithub}
+              onClick={() => socialAction("github")}
+            />
+            <AuthSocialButton
+              icon={BsGoogle}
+              onClick={() => socialAction("google")}
+            />
+          </div>
+        </div>
+        <div
+          className='
+            flex
+            gap-2
+            justify-center
+            text-sm
+            mt-6
+            px-2
+            text-gray-500'
+        >
+          <div>
+            {variant === "LOGIN"
+              ? "New to Messenger"
+              : "Already have an account ?"}
+          </div>
+          <div onClick={toggleVariant} className='underline cursor-pointer'>
+            {variant === "LOGIN" ? "Create an account" : "Login"}
+          </div>
+        </div>
       </div>
     </div>
   );
